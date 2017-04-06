57c305114e43cba6b2f5ed02bc935a758ff9beec
邹前立 <Reborn@rebornsMacBook-Pro.local>
2017-04-06 08:57:24 +0800

    Initial Commit

---
+++ SimpleScoreManageSystem2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleScoreManageSystem2
+//
+//  Created by 邹前立 on 2017/4/6.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
